eb205356cdae8c0733659d965b9ceee3c43cd213
Osman <osmanah7@gmail.com>
2017-01-31 14:34:30 +0100

    Aout de slider

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+
+// slider
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+
+
+
+// map js
+
 var myLatlng = new google.maps.LatLng(37.7552464,-122.4185384);
 var myOptions = {
     zoom: 16,
