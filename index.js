29d8e059785b80b327e18155a120ca48aefd83bb
Ghunbot Dawit <ghunbotdawit@MacBook-Air-de-Ghunbot.local>
2017-01-31 14:18:34 +0100

    skelet de resto boufle

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+var myLatlng = new google.maps.LatLng(37.7552464,-122.4185384);
+var myOptions = {
+    zoom: 16,
+    center: myLatlng,
+    scrollwheel: false,
+    mapTypeId: 'satellite', //google.maps.MapTypeId.ROADMAP,
+    styles: []
+  };
+  var map = new google.maps.Map(document.getElementById('map'), myOptions);
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: "here you are"
+  });
+var affichier = document.getElementById('PANIER');
