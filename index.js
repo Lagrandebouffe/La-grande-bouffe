f12fee65f71203bc4b1dbaa7c04b3ffb8181bf46
Adamska <adams.roy27@gmail.com>
2017-02-06 16:48:04 +0100

    ajustements

--- index.js
+++ index.js
@@ -18,80 +18,21 @@ function carousel() {
         x[i].style.display = "none";
     }
     myIndex++;
-<<<<<<< HEAD
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 3000); // Change image every 3 seconds
-=======
     if (myIndex > x.length) {
         myIndex = 1
     }
     x[myIndex - 1].style.display = "block";
     setTimeout(carousel, 3000); // Change image every 2 seconds
->>>>>>> 825ce8352c99f1e52a61ad2851bd11b6dac90b99
 }
 
 
 var main = document.getElementById('mains');
-<<<<<<< HEAD
-var h11 = document.createElement("h1");
-var hh = h11.innerHTML = contenu.name + '<h1 id = h1-description></h1>';
-main.appendChild (h11);
-=======
->>>>>>> 825ce8352c99f1e52a61ad2851bd11b6dac90b99
 
 //La description main
 var p1 = document.createElement("p");
 p1.classList.add("description");
-<<<<<<< HEAD
-p1.innerHTML = contenu.description+'<p id = p-description></p>';
-main.appendChild (p1);
-
-// Récupérer données pour la carte des menus
-
-var contenuMenu = document.getElementById("container-menu");
-var menuCarte = contenu.carte;
-
-function creationMenu () {
-  for (var i = 0; i < menuCarte.length; i++) {
-
-    var menuElt = document.createElement("div");
-    // var borderImage = document.createElement("div");
-    var imageElt = document.createElement("img");
-    var contentElt = document.createElement("div");
-    var nomElt = document.createElement("h3");
-    var descriptionElt = document.createElement("p");
-    var btn = document.createElement("button");
-
-    menuElt.classList.add("menus", "row");
-    // borderImage.classList.add("col-xs-12", "col-sm-3")
-    // borderImage.style.border = "2px black solid";
-    // borderImage.style.padding = "10px";
-    // borderImage.style.backgroundColor = "rgb(0,0,0)";
-    imageElt.src = menuCarte[i].image;
-    imageElt.classList.add("menuImage", "image-responsive", "col-xs-12", "col-sm-offset-1", "col-sm-4");
-    contentElt.classList.add("content", "col-xs-12", "col-sm-offset-1", "col-sm-5", "col-1-offset-1");
-    nomElt.textContent = menuCarte[i].name; // N.textContent = innerHTML
-    descriptionElt.textContent = menuCarte[i].description;
-    btn.textContent = "Mange moi";
-
-    // borderImage.appendChild(imageElt);
-    menuElt.appendChild(imageElt);
-    // menuElt.appendChild(imageElt);
-
-    contentElt.appendChild(nomElt);
-    contentElt.appendChild(descriptionElt);
-    contentElt.appendChild(btn);
-    menuElt.appendChild(contentElt);
-    contenuMenu.appendChild(menuElt);
-  }
-}
-
-creationMenu();
-=======
 p1.innerHTML = contenu.description + '<p id = p-description></p>';
 main.appendChild(p1);
->>>>>>> 825ce8352c99f1e52a61ad2851bd11b6dac90b99
 
 // partie footer
 
@@ -112,31 +53,3 @@ foot.appendChild(newspan3);
 var newspan4 = document.createElement('span');
 contentspan = newspan4.innerHTML = contenu.ville + '<span></span><br>';
 foot.appendChild(newspan4);
-<<<<<<< HEAD
-
-function myFunction(){
-  document.getElementById("ACCUEIL").innerHTML = "this is me what do u wanna do";
-}
-
-//
-// var foot = document.getElementById('footer-text');
-// var footSpan = document.createElement("span");
-// var ContenueSpan = footSpan.innerHTML = contenu.baseline+'<span></span><br>';
-// foot.appendChild (footSpan);
-//
-// var foot = document.getElementById('footer-text');
-// var footSpan = document.createElement("span");
-// var ContenueSpan = footSpan.innerHTML = contenu.addresse+'<span></span><br>';
-// foot.appendChild (footSpan);
-//
-// var foot = document.getElementById('footer-text');
-// var footSpan = document.createElement("span");
-// var ContenueSpan = footSpan.innerHTML = contenu.codePostale+'<span></span><br>';
-// foot.appendChild (footSpan);
-//
-// var foot = document.getElementById('footer-text');
-// var footSpan = document.createElement("span");
-// var ContenueSpan = footSpan.innerHTML = contenu.ville+'<span></span><br>';
-// foot.appendChild (footSpan);
-=======
->>>>>>> 825ce8352c99f1e52a61ad2851bd11b6dac90b99
